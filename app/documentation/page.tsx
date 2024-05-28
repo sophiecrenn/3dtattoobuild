c936691987a9ca217acfdf54dc091f2e0a78bbca
sophiecrenn <sophie.crenn.fox@gmail.com>
2024-05-28 09:17:08 +0200

    buil doc
    buil doc

--- app/documentation/page.tsx
+++ app/documentation/page.tsx
@@ -1,54 +1,53 @@
-'use client'
+'use client';
 
-import FrenchDocumentation from "@/app/documentation/French";
-import EnglishDocumentation from "@/app/documentation/English";
-import { useState } from "react";
-import "./documentation.css";
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import './documentation.css';
+
+const FrenchDocumentation = dynamic(() => import('./French'));
+const EnglishDocumentation = dynamic(() => import('./English'));
 
 export default function Documentation() {
-const [showCards, setShowCards] = useState(false);
-const [language, setLanguage] = useState("French");
+  const [showCards, setShowCards] = useState(false);
+  const [language, setLanguage] = useState('French');
 
-// Fonction pour basculer l'état des cartes
-const toggleCards = () => {
-  // Si les cartes sont déjà affichées, les cacher en mettant showCards à false
-  if (showCards) {
-    setShowCards(false);
-  } else {
-    setShowCards(true);
-  }
-};
+  const toggleCards = () => {
+    console.log('Toggling showCards:', !showCards);
+    setShowCards(!showCards);
+  };
 
-return (
-  <section className="documentationContainer" id="documentation">
-    <h2 className="docTitle">DOCUMENTATION</h2>
-    <div className="languageButtons">
-      <button
-        id="frenchButton"
-        onClick={() => {
-          setLanguage("French");
-          toggleCards(); // Utiliser la fonction toggleCards au lieu de setShowCards(true)
-        }}
-      >
-        Version française
-      </button>
-      <button
-        id="englishButton"
-        onClick={() => {
-          setLanguage("English");
-          toggleCards(); // Utiliser la fonction toggleCards au lieu de setShowCards(true)
-        }}
-      >
-        English version
-      </button>
-    </div>
-    <div className="firstContainer">
-      {language === "French" ? (
-        <FrenchDocumentation showCards={showCards} setShowCards={setShowCards} />
-      ) : (
-        <EnglishDocumentation showCards={showCards} setShowCards={setShowCards} />
-      )}
-    </div>
-  </section>
-);
+  return (
+    <section className="documentationContainer" id="documentation">
+      <h2 className="docTitle">DOCUMENTATION</h2>
+      <div className="languageButtons">
+        <button
+          id="frenchButton"
+          onClick={() => {
+            console.log('Switching to French');
+            setLanguage('French');
+            toggleCards();
+          }}
+        >
+          Version française
+        </button>
+        <button
+          id="englishButton"
+          onClick={() => {
+            console.log('Switching to English');
+            setLanguage('English');
+            toggleCards();
+          }}
+        >
+          English version
+        </button>
+      </div>
+      <div className="firstContainer">
+        {language === 'French' ? (
+          <FrenchDocumentation showCards={showCards} setShowCards={setShowCards} />
+        ) : (
+          <EnglishDocumentation showCards={showCards} setShowCards={setShowCards} />
+        )}
+      </div>
+    </section>
+  );
 }
